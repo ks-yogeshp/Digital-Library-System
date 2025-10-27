39a081f4d6ddc5d2d5e962d94724bd1e08356ca6
ks-yogeshp <yogesh.patel@kalkani.in>
2025-10-27 13:40:42 +0530

    Created database configuration file

---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import { registerAs } from "@nestjs/config";
+
+export default registerAs('database', ()=> ({
+    port: parseInt(process.env.DATABASE_PORT ?? '5432' ,10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    name: process.env.DATABASE_NAME,
+    synchronize: process.env.DATABASE_SYNC === 'true' ? true : false,
+    autoLoadEntities: process.env.DATABASE_AUTOLOAD === 'true' ? true : false,
+
+}))
