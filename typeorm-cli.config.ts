4caa56c92106f1db6452f80d2f7cb43cff7f0d9d
ks-yogeshp <yogesh.patel@kalkani.in>
2025-10-28 14:00:21 +0530

    Added typeorm config file for migrations

---
+++ typeorm-cli.config.ts
@@ -0,0 +1,16 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+
+dotenv.config();
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: ['**/*.entity.js'],
+  migrations: ['**/migrations/*.js'],
+});
