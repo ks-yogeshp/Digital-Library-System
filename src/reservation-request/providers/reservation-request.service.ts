0c076ccb55734360322b018e40cb7a45823b5157
ks-yogeshp <yogesh.patel@kalkani.in>
2025-11-06 17:48:46 +0530

    feat(reservation-request): add Dto and upadated controller, service to handle reservataion-request

--- src/reservation-request/providers/reservation-request.service.ts
+++ src/reservation-request/providers/reservation-request.service.ts
@@ -1,13 +1,17 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { Any, Repository } from 'typeorm';
+import { BadRequestException, Body, Injectable } from '@nestjs/common';
+import { Any, DataSource, LessThan, MoreThan, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ReservationRequest } from '../reservation-request.entity';
 import { CreateReservationRequestDto } from '../dtos/create-reservation-request.dto';
 import { Book } from 'src/books/book.entity';
 import { User } from 'src/users/user.entity';
 import { requestStatus } from '../enums/requestStatus.enum';
-import { ne, tr } from '@faker-js/faker';
 import { MailService } from 'src/mail/providers/mail.service';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { CheckoutReservationRequestDto } from '../dtos/checkout-reservation-request.dto';
+import { BorrowRecord } from 'src/borrow-record/borrow-record.entity';
+import { CancelReservationRequestDto } from '../dtos/cancel-reservation-request.dto';
+import { availabilityStatus } from 'src/books/enums/availibityStatus.enum';
 
 @Injectable()
 export class ReservationRequestService {
@@ -22,6 +26,8 @@ export class ReservationRequestService {
         private readonly userRepository: Repository<User>,
 
         private readonly mailService: MailService,
+
+        private readonly dataSource: DataSource
 
     ){}
 
@@ -83,12 +89,77 @@ export class ReservationRequestService {
             await this.mailService.sendReservationApproved(nextReservation);
             await this.reservationRequestRepository.save(nextReservation);
             return true
-
         }
         else{
-            return false;
+            book.availabilityStatus = availabilityStatus.AVAILABLE;
+            await this.bookRepository.save(book);
         }
+    }
 
+    public async checkoutBook(
+       checkoutReservationRequestDto: CheckoutReservationRequestDto
+    ){
+        const now = new Date();
+        const reservation = await this.reservationRequestRepository.findOne({
+            where:{
+                book:{
+                    id:checkoutReservationRequestDto.bookId
+                },
+                user:{
+                    id:checkoutReservationRequestDto.userId
+                },
+                requestStatus: requestStatus.APPROVED,
+                active_until: MoreThan(now)
+            },relations:{
+                user:true,
+                book:true
+            }
+        })
+        if(!reservation){
+            throw new BadRequestException();
         }
+        now.setHours(0, 0, 0, 0);
+        now.setDate(now.getDate() - 14)
+        const dueDate = new Date(now);
+        dueDate.setDate(dueDate.getDate() + checkoutReservationRequestDto.days);
+        let record = this.dataSource.getRepository(BorrowRecord).create({
+            book: reservation.book,
+            user: reservation.user,
+            borrowDate: now,
+            dueDate: dueDate,
+        })
+        await this.dataSource.transaction(async (manager) => {
+            await manager.save(record)
+            reservation.requestStatus= requestStatus.FULFILLED;
+            await manager.save(reservation);
+        });
+
+        return record;
+    }
+
+    public async cancelResrvation(cancelResvationRequestDto: CancelReservationRequestDto){
+        const reservation = await this.reservationRequestRepository.findOne({
+            where:{
+                book:{
+                    id:cancelResvationRequestDto.bookId
+                },
+                user:{
+                    id:cancelResvationRequestDto.userId
+                },
+                requestStatus: Any[requestStatus.APPROVED,requestStatus.PENDING],
+            },relations:{
+                user:true,
+                book:true
+            }
+        })
+        if(!reservation){
+            throw new BadRequestException();
+        }
+        if(reservation.requestStatus === requestStatus.APPROVED){
+            this.nextReservation(reservation.book);
+        }
+        reservation.requestStatus = requestStatus.CANCELLED;
+        return await this.reservationRequestRepository.save(reservation);
+    }
 
 }
--- src/reservation-request/reservation-request.controller.ts
+++ src/reservation-request/reservation-request.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ReservationRequestService } from './providers/reservation-request.service';
 import { CreateReservationRequestDto } from './dtos/create-reservation-request.dto';
+import { CheckoutReservationRequestDto } from './dtos/checkout-reservation-request.dto';
+import { CancelReservationRequestDto } from './dtos/cancel-reservation-request.dto';
 
 @Controller('reservation-request')
 export class ReservationRequestController {
@@ -12,15 +14,25 @@ export class ReservationRequestController {
     ){}
 
     @Post()
-    public createBook(
+    public createReservationResquest(
         @Body() createReservationRequestDto: CreateReservationRequestDto,
     ){
         return this.reservationRequestService.createReservation(createReservationRequestDto);
     }   
-    
-    @Get()
-    public next(){
-        return this.reservationRequestService.get();
+
+    @Post('reserve')
+    public reserveBook( 
+        @Body() checkoutReservationRequestDto: CheckoutReservationRequestDto 
+    ){
+        return this.reservationRequestService.checkoutBook(checkoutReservationRequestDto);
     }
+
+    @Post('cancel')
+    public cancelReservation(
+        @Body() cancelReservationRequestDto: CancelReservationRequestDto 
+    ){
+        return this.reservationRequestService.cancelResrvation(cancelReservationRequestDto);
+    }
+
 
 }
