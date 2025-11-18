a3bacb26f174e80d5e3fdc4b4a339b1e74e02e91
ks-yogeshp <yogesh.patel@kalkani.in>
2025-11-18 12:17:26 +0530

    refactor(config): use envalid for configuration validation

--- src/config/mail.config.ts
+++
@@ -1,7 +0,0 @@
-import { registerAs } from "@nestjs/config";
-
-export default registerAs('mail', ()=> ({
-    host: process.env.MAIL_HOST,
-    smtpUsername: process.env.SMTP_USERNAME,
-    smtpPassword: process.env.SMTP_PASSWORD,
-}))
