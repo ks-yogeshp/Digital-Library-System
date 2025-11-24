d901f49ac2f624b0c09d71083e4ef76cdb7d74bc
ks-yogeshp <yogesh.patel@kalkani.in>
2025-11-24 10:22:43 +0530

    feat(database): add abstract entities for soft delete functionality

---
+++ src/database/entities/abstract.entity.ts
@@ -0,0 +1,12 @@
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+export class AbstractEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
