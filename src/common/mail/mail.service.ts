e3689f011d0a4ae60b176a143bd1e4feff29ce89
ks-yogeshp <yogesh.patel@kalkani.in>
2025-11-26 16:28:17 +0530

    feat(mail): enhance MailService to include monthly report functionality and refactor methods to use DataSource for fetching records

--- src/common/mail/mail.service.ts
+++ src/common/mail/mail.service.ts
@@ -1,5 +1,6 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable, Logger } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 import { CONFIG } from 'src/config';
 import { BorrowRecord } from 'src/database/entities/borrow-record.entity';
@@ -7,9 +8,16 @@ import { ReservationRequest } from 'src/database/entities/reservation-request.en
 
 @Injectable()
 export class MailService {
-  constructor(private readonly mailerService: MailerService) {}
+  constructor(
+    private readonly mailerService: MailerService,
+    private readonly dataSource: DataSource
+  ) {}
 
-  public async sendRemainder(borrowRecord: BorrowRecord, daysDiff: number) {
+  public async sendRemainder(data: { id: number; daysDiff: number }) {
+    const { id, daysDiff } = data;
+    const borrowRecord = await this.dataSource.getRepository(BorrowRecord).findOneOrFail({
+      where: { id },
+    });
     const isOverdue = daysDiff < 0;
     const book = await borrowRecord.book;
     const user = await borrowRecord.user;
@@ -34,7 +42,11 @@ export class MailService {
     }
   }
 
-  public async sendReservationApproved(reservation: ReservationRequest) {
+  public async sendReservationApproved(data: { id: number }) {
+    const { id } = data;
+    const reservation = await this.dataSource.getRepository(ReservationRequest).findOneOrFail({
+      where: { id },
+    });
     const book = await reservation.book;
     const user = await reservation.user;
 
@@ -55,6 +67,30 @@ export class MailService {
       Logger.log({ msg: `Reservation approval sent to ${user.email}` });
     } catch (error) {
       Logger.error({ msg: `Failed to send reservation approval to ${user.email}`, error });
+    }
+  }
+  public async sendMonthlyReport(data: { to: string; buffer: any; reportMonth: string }) {
+    const { to, buffer, reportMonth } = data;
+    const file = Buffer.from(buffer);
+    try {
+      await this.mailerService.sendMail({
+        to,
+        subject: 'Monthly Borrow Records Report',
+        template: 'monthly-report',
+        context: {
+          month: new Date().toLocaleString('default', { month: 'long' }),
+          libraryUrl: CONFIG.APP_URL,
+        },
+        attachments: [
+          {
+            filename: `monthly-borrow-report-${reportMonth}.xlsx`,
+            content: file,
+          },
+        ],
+      });
+      Logger.log(`Monthly borrow report emailed to ${to}`);
+    } catch (error) {
+      Logger.error(`Failed to send monthly report to ${to}`, error);
     }
   }
 }
