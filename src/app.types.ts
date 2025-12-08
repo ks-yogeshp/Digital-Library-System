39497f77acb32920afacba0157380abfd252078d
ks-yogeshp <yogesh.patel@kalkani.in>
2025-12-08 11:16:21 +0530

    feat(types): update entity imports to use new Mongoose schema structure and add User type

--- src/app.types.ts
+++ src/app.types.ts
@@ -1,7 +1,9 @@
-import { Author } from './database/entities/author.entity';
-import { Book } from './database/entities/book.entity';
+import { Author } from './database/schemas/author.schema';
+import { Book } from './database/schemas/book.schema';
+import { User } from './database/schemas/user.schema';
 
 export type MyEntityMap = {
   Book: Book;
   Author: Author;
+  User: User;
 };
